d8ebe7ce3c7308504dab4115c824963342a83120
Master317 <alireza.khodabakhshi.s@gmail.com>
2019-09-13 20:40:32 +0430

    create shopPage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import {Route,Switch} from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
-import HomePage from './pages/homepage/homepage.component';
-const HatsPage=()=>(
-  <div><h1>Hats Page</h1></div>
-)
-function App() {
-  return <div>
-<Switch>
-  <Route exact path='/' component={HomePage}/>
-<Route path='/hats' component={HatsPage}/>
-</Switch>
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
-  </div>;
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
