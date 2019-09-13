3da71cc4f9597b1607141923a459952aa1184771
Master317 <alireza.khodabakhshi.s@gmail.com>
2019-09-13 19:59:38 +0430

    withRouter

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './menu-item.styles.scss';
-
-const MenuItem=({title,imageUrl,size})=>(
-    <div   className={`${size} menu-item`}>
+import {withRouter} from 'react-router-dom';
+const MenuItem=({title,imageUrl,size,history,linkUrl,match})=>(
+    <div   className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
         <div style={{backgroundImage:`url(${imageUrl})`}} className='background-image' />
             <div className="content">
                 <h1 className="title">{title.toUpperCase()}</h1>
@@ -10,4 +10,4 @@ const MenuItem=({title,imageUrl,size})=>(
             </div>
         </div>
 )
-export default MenuItem;
+export default withRouter(MenuItem) ;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
-import { directive } from "@babel/types";
+
 const HomePage = () => (
   <div className="homepage">
     <Directory />
